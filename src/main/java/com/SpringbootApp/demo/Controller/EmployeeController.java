196acbbc69f169f6b090a9b2770dc7418b84e0cd
tamangnabin <86223380+tamangnabin@users.noreply.github.com>
2024-11-10 12:04:40 -0600

    commented in controller class

--- src/main/java/com/SpringbootApp/demo/Controller/EmployeeController.java
+++ src/main/java/com/SpringbootApp/demo/Controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+    // This is a controller class
     @GetMapping(value = "hello")
     public String sayHi(){
         return "Welcome to Nepal";
